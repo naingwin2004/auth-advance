9ed5ccb7ace8e6f88760003a2d3f0bdabe546d6d
naingwin2004 <naingwin.dev@gmail.com>
2024-11-01 23:37:45 +0630

    checkauth & frontend setup (bug fix resetpassword)

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js";
 
 import { connectDB } from "./db/connectDB.js";
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json()); // allow us to parse incomming requests : req.body
-
+app.use(cookieParser()) // allow us to parse cookie
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
